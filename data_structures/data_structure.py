3cae70d0765f6146765a52435e24052ea62f934d
RomeroGabriel <gabrielromerods@hotmail.com>
2022-07-27 18:56:22 -0300

    List

--- data_structures/data_structure.py
+++ data_structures/data_structure.py
@@ -1,4 +1,6 @@
-# tuple t
+from audioop import reverse
+
+
 print('Starting with TUPLES!!!')
 print()
 t = 'AA', '2011-06-07', 100, 32.2
@@ -27,3 +29,26 @@ print('Can be nested to other tuple')
 u = t, (1, 2, 3, 4, 5)
 print(f'Result of two tuple nested {u}')
 print(f'Length of u: {len(u)}')
+
+print()
+print()
+
+print('Starting with LISTS!!!')
+print()
+fruits = ['orange', 'apple', 'pear', 'banana', 'kiwi', 'apple', 'banana']
+print(f"Tuple exaple is: fruits = {fruits}")
+print(f'fruits count: {fruits.count("apple")}')
+print(f'banana index: {fruits.index("banana")}')
+print(f'reverse fruits {fruits.reverse()} CHANGING THE ORIGINAL LIST {fruits}')
+print('**')
+print('To reverse a list without change original list, is necessary to copy (with other reference object) and reverse or use slice(better)')
+print('Link for more information: https://stackoverflow.com/questions/41777333/how-to-reverse-a-list-without-modifying-the-original-list-in-python')
+print('**')
+print(f'add element grape: {fruits.append("grape")} fruits is {fruits}')
+print(f'sort fruits {fruits.sort()} CHANGING THE ORIGINAL LIST {fruits}')
+print(f'remove last object from fruits using pop and retorn the element: {fruits.pop()}. Fruits now is: {fruits}')
+
+print()
+print()
+
+print('Starting with SETS!!!')
